6df72112bc17556ccdcaf057762b548a1d073fa4
Yuki Yoshida <sence.service@gmail.com>
2023-09-26 18:28:32 +0900

    update

--- components/templates/seatMap.tsx
+++ components/templates/seatMap.tsx
@@ -4,6 +4,7 @@ import useIsHeaderGlobal from "@/globalstates/isHeader";
 import useIsFooterGlobal from "@/globalstates/isFooter";
 import useIsCardGlobal from "@/globalstates/isCard";
 import Image from "next/image";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 export default function SeatMap() {
   const [isHeader, setIsHeader] = useIsHeaderGlobal();
@@ -11,44 +12,47 @@ export default function SeatMap() {
   const [isCard, setIsCard] = useIsCardGlobal();
 
   return (
-    <div className="h-full w-full overflow-scroll">
-      <section
-        id="map"
-        className="touch-manipulation"
-        onClick={() => {
-          if (isHeader) setIsHeader(false);
-          if (isFooter) setIsFooter(false);
-          if (isCard) setIsCard(false);
-        }}
-      >
-        {seatMap.map((seat, index) => {
-          switch (seat.type) {
-            case "seat":
-              return (
-                <Seat key={index} id={seat.id} area={seat.area}>
-                  {seat.id.toLocaleUpperCase()}
-                </Seat>
-              );
-            case "object":
-              return (
-                <Image
-                  key={index}
-                  width={30}
-                  height={30}
-                  className={seat.area + " !w-full !h-full"}
-                  src={seat.objectUrl}
-                  alt=""
-                />
-              );
-            case "text":
-              return (
-                <Seat key={index} id={seat.id} area={seat.area}>
-                  {seat.body}
-                </Seat>
-              );
-          }
-        })}
-      </section>
-    </div>
+    <TransformWrapper initialScale={1}>
+      <TransformComponent>
+        <div className="flex justify-center items-center w-[200dvw] h-[100dvh]">
+          <section
+            id="map"
+            onClick={() => {
+              if (isHeader) setIsHeader(false);
+              if (isFooter) setIsFooter(false);
+              if (isCard) setIsCard(false);
+            }}
+          >
+            {seatMap.map((seat, index) => {
+              switch (seat.type) {
+                case "seat":
+                  return (
+                    <Seat key={index} id={seat.id} area={seat.area}>
+                      {seat.id.toLocaleUpperCase()}
+                    </Seat>
+                  );
+                case "object":
+                  return (
+                    <Image
+                      key={index}
+                      width={30}
+                      height={30}
+                      className={seat.area + " !w-full !h-full"}
+                      src={seat.objectUrl}
+                      alt=""
+                    />
+                  );
+                case "text":
+                  return (
+                    <Seat key={index} id={seat.id} area={seat.area}>
+                      {seat.body}
+                    </Seat>
+                  );
+              }
+            })}
+          </section>
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
   );
 }
