5de29135352d2018511338f22463fe2770e53c4f
Koala <sence.service@gmail.com>
2023-12-21 12:29:22 +0900

    update

--- components/build/buildMenu.tsx
+++ components/build/buildMenu.tsx
@@ -3,9 +3,9 @@
 export default function Build() {
   return (
     <>
-      <div className="absolute left-0 top-0 z-40 flex h-[100dvh] w-[100dvw] items-center justify-center  bg-white/100 p-10 text-white">
+      <div className="absolute left-0 top-0 z-40 flex h-[100dvh] w-[100dvw] items-center justify-center  bg-black/100 p-10 text-white">
         <div
-          className="flex h-[90%] w-full max-w-[1000px] flex-col items-center justify-start overflow-y-scroll"
+          className="flex h-full w-full items-center justify-center overflow-y-scroll"
           onClick={(e) => e.stopPropagation()}
         >
           <span className="loading loading-bars loading-lg"></span>
