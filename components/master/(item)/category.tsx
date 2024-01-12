9078c8859e9ee32485a1b800159ceaee31a59b4e
Koala <sence.service@gmail.com>
2024-01-12 17:58:34 +0900

    update

--- components/master/(item)/category.tsx
+++ components/master/(item)/category.tsx
@@ -1,87 +1,124 @@
 "use client";
 
 import Control from "@/components/master/(component)/control";
-import { useState } from "react";
+import Border from "@/components/master/border";
+import Toggle from "@/components/templates/toggle4";
+import Image from "next/image";
 
-export default function ItemCategoryList() {
-  const [activeTab, setActiveTab] = useState(0);
+export default function ItemCategoryLists() {
   return (
     <>
       <Control>
-        <div className="h-full w-[1000px] bg-white p-4">
-          <div className="tabs">
-            <a
-              className={`tab tab-lg mr-1 w-[8em] rounded-t-xl ${
-                activeTab == 0
-                  ? "tab-active bg-black text-white"
-                  : "tab-lifted bg-secondary text-black"
-              }`}
-              onClick={() => setActiveTab(0)}
-            >
-              メイン
-            </a>
-            <a
-              className={`tab tab-lg mr-1 w-[8em] rounded-t-xl ${
-                activeTab == 1
-                  ? "tab-active bg-black text-white"
-                  : "tab-lifted bg-secondary text-black"
-              }`}
-              onClick={() => setActiveTab(1)}
-            >
-              VIP
-            </a>
-            <a
-              className={`tab tab-lg mr-1 w-[8em] rounded-t-xl ${
-                activeTab == 2
-                  ? "tab-active bg-black text-white"
-                  : "tab-lifted bg-secondary text-black"
-              }`}
-              onClick={() => setActiveTab(2)}
-            >
-              案内所/外販
-            </a>
-            <a className="tab tab-lifted tab-lg w-[8em] rounded-t-xl bg-neutral-400 text-black">
-              +
-            </a>
-          </div>
-          <div className="mt-[-1px] h-[400px] w-[800px] rounded-b-xl rounded-r-xl bg-black text-white">
-            <p>
-              {activeTab == 0
-                ? "メイン"
-                : activeTab == 1
-                ? "VIP"
-                : activeTab == 2
-                ? "その他"
-                : ""}
-            </p>
-            <span
-              className={
-                "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
-              }
-            >
-              鏡月
-            </span>
-            <span
-              className={
-                "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
-              }
-            >
-              鏡月
-            </span>
-            <span
-              className={
-                "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
-              }
-            >
-              鏡月
-            </span>
-            <span
-              className={
-                "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
-              }
-            >
-              鏡月
-            </span>
+        <div className="flex">
+          <Border
+            className="my-2 w-[300px]"
+            size="p-4 flex flex-col overflow-scroll"
+            black
+          >
+            <p className="text-accent">大カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <p className="text-accent">小カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <Border rounded="rounded-full" size="h-[40px] w-[40px] p-[12px]">
+              <Image
+                src={"/assets/add.svg"}
+                width={26}
+                height={26}
+                className="!h-full !w-full"
+                alt=""
+              />
+            </Border>
+          </Border>
+          <Border
+            className="my-2 w-[300px]"
+            size="p-4 flex flex-col overflow-scroll"
+            black
+          >
+            <p className="text-accent">大カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <p className="text-accent">小カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <Border rounded="rounded-full" size="h-[40px] w-[40px] p-[12px]">
+              <Image
+                src={"/assets/add.svg"}
+                width={26}
+                height={26}
+                className="!h-full !w-full"
+                alt=""
+              />
+            </Border>
+          </Border>
+          <Border
+            className="my-2 w-[300px]"
+            size="p-4 flex flex-col overflow-scroll"
+            black
+          >
+            <p className="text-accent">大カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <p className="text-accent">小カテゴリー</p>
+            <input
+              type="text"
+              className="mb-2 h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <input
+              type="text"
+              className="h-[30px] w-[6rem] rounded-md px-2 text-sm"
+            />
+            <Border rounded="rounded-full" size="h-[40px] w-[40px] p-[12px]">
+              <Image
+                src={"/assets/add.svg"}
+                width={26}
+                height={26}
+                className="!h-full !w-full"
+                alt=""
+              />
+            </Border>
+          </Border>
+          <div className="ml-6 h-[45px] w-[45px]">
+            <Border rounded="rounded-full" size="h-[40px] w-[40px] p-[12px]">
+              <Image
+                src={"/assets/add.svg"}
+                width={26}
+                height={26}
+                className="!h-full !w-full"
+                alt=""
+              />
+            </Border>
           </div>
         </div>
       </Control>
--- components/master/(item)/orderSet.tsx
+++ components/master/(item)/orderSet.tsx
@@ -1,9 +1,183 @@
+"use client";
+
 import Control from "@/components/master/(component)/control";
+import { useState } from "react";
+import Border from "../border";
 
 export default function OrderSet() {
+  const [activeTab, setActiveTab] = useState(0);
   return (
     <>
-      <Control>オーダー配置</Control>
+      <Control>
+        <Border
+          className="my-2 w-full"
+          size="p-4 flex flex-col overflow-scroll"
+          black
+        >
+          <div className="mb-4 flex">
+            <span
+              className={
+                "m-4 flex max-w-[160px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+              }
+            >
+              フード
+            </span>
+            <span
+              className={
+                "m-4 flex max-w-[160px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+              }
+            >
+              ボトル
+            </span>
+            <span
+              className={
+                "m-4 flex max-w-[160px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+              }
+            >
+              サービス
+            </span>
+          </div>
+          <div className="tabs">
+            <a
+              className={`tab-lg tab -ml-[4px] mr-1 w-[8em] rounded-t-xl ${
+                activeTab == 0
+                  ? "tab-active bg-primary text-white"
+                  : "tab-lifted bg-secondary text-black"
+              }`}
+              onClick={() => setActiveTab(0)}
+            >
+              焼酎
+            </a>
+            <a
+              className={`tab-lg tab mr-1 w-[8em] rounded-t-xl ${
+                activeTab == 1
+                  ? "tab-active bg-primary text-white"
+                  : "tab-lifted bg-secondary text-black"
+              }`}
+              onClick={() => setActiveTab(1)}
+            >
+              果実酒
+            </a>
+            <a
+              className={`tab-lg tab mr-1 w-[8em] rounded-t-xl ${
+                activeTab == 2
+                  ? "tab-active bg-primary text-white"
+                  : "tab-lifted bg-secondary text-black"
+              }`}
+              onClick={() => setActiveTab(2)}
+            >
+              日本酒
+            </a>
+            <a className="tab-lifted tab-lg tab w-[8em] rounded-t-xl bg-neutral-400 text-black">
+              +
+            </a>
+            <a className="tab-lifted tab-lg tab w-[8em] rounded-t-xl text-black"></a>
+            <a className="tab-lifted tab-lg tab w-[8em] rounded-t-xl text-black"></a>
+            <a className="tab-lifted tab-lg tab w-[8em] rounded-t-xl text-black"></a>
+          </div>
+          <div className="mt-[-1px] h-[460px] w-[800px] rounded-b-xl rounded-r-xl bg-primary p-4 text-white">
+            {/* <p>
+              {activeTab == 0
+                ? "メイン"
+                : activeTab == 1
+                ? "VIP"
+                : activeTab == 2
+                ? "その他"
+                : ""}
+            </p> */}
+            <Border
+              className="my-2 w-full"
+              size="p-4 flex overflow-scroll flex-wrap"
+              black
+            >
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+              <span
+                className={
+                  "m-4 flex max-w-[130px] cursor-pointer items-center justify-center rounded-xl bg-blue-500 bg-gradient-to-b from-[#c9f3f3] from-5% via-[#86b2b2] via-10% to-[#597777] p-6 text-xl leading-4 tracking-wider"
+                }
+              >
+                鏡月
+              </span>
+            </Border>
+          </div>
+        </Border>
+      </Control>
     </>
   );
 }
